77990f1548ed9eec41fece0452c08ec258f34eac
AndrewRiceMGW <andrew.m.f.rice@gmail.com>
2026-03-24 17:53:46 +0000

    updated version

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Enables offline functionality and app-like experience
  */
 
-const CACHE_NAME = "trick-generator-v0.9.0-beta";
+const CACHE_NAME = "trick-generator-v0.9.1-beta";
 const urlsToCache = [
 	"/",
 	"/index.html",
